bbe6b0191e28f73663fc3300446c8e0c9ed9ee59
afkcodes <ashish.kmr@outlook.com>
2023-07-19 22:43:58 +0530

    updated exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
+import { Image } from 'react-native';
+import MiniPlayer from './MiniPlayer';
+import SText from './SText/SText';
 import SView from './SView/SView';
+import Tile from './Tile/Tile';
+import TitleSubtitle from './TitleSubtitle';
 
-export { SView };
+export { Image, MiniPlayer, SText, SView, Tile, TitleSubtitle };
